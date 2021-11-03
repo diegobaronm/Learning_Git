a6717fad8b9915d852730cd15b6fe364df2cbb17
Diego Baron <diegoa_baronm@hotmail.com>
2021-11-03 23:36:09 +0000

    Adding comments to the code.

--- Script.py
+++ Script.py
@@ -1,7 +1,9 @@
+# Importing libraries
 import sys
 
+# Getting the input from command
 n=int(sys.argv[1])
-
+# Deciding if the number is even or odd by taking its modulus 2
 if n%2==0:
 	print("Es par!")
 else :
