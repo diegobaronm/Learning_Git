79deaac0f5bee891cbb4e066454f3acb5471ef6e
Diego Baron <diegoa_baronm@hotmail.com>
2021-11-03 23:30:48 +0000

    Starting my development branch

--- Script.py
+++ Script.py
@@ -2,7 +2,10 @@ import sys
 
 n=int(sys.argv[1])
 
-if n%2==0:
-	print("Es par!")
-else :
-	print("Es impar...")
+def odd_vs_even(n):
+	if n%2==0:
+		print("Es par!")
+	else :
+		print("Es impar...")
+
+odd_vs_even(n)
