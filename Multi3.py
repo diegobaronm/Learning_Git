9651df6860101e3f9ca12b5a7c7e66cdd7510820
Diego Baron <diegoa_baronm@hotmail.com>
2021-11-04 16:39:40 +0000

    Adding another script to determine if a number is a multiple of 3.

---
+++ Multi3.py
@@ -0,0 +1,14 @@
+# Importing libraries
+import sys
+
+# Getting the input from command
+n=int(sys.argv[1])
+# Defining function to decide parity of a number
+def Multi3(n):
+	if n%3==0:
+		print("El numero "+str(n)+" es multiplo de 3!")
+	else :
+		print("El numero "+str(n)+" no es multiplo de 3...")
+
+# This is the main part of the program!
+Multi3(n)
