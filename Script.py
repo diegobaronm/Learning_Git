8238f7c8b91895f7843ed17541e63016228419df
Diego Baron <diegoa_baronm@hotmail.com>
2021-11-04 16:13:25 +0000

    Adding more comments to the code.

--- Script.py
+++ Script.py
@@ -4,11 +4,11 @@ import sys
 # Getting the input from command
 n=int(sys.argv[1])
 # Defining function to decide parity of a number
-
 def odd_vs_even(n):
 	if n%2==0:
 		print("El numero "+str(n)+" es par!")
 	else :
 		print("El numero "+str(n)+" es impar...")
 
+# This is the main part of the program!
 odd_vs_even(n)
